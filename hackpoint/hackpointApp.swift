029f0596452cbc05c7a7cacbe0c2207a43b86c68
Fedorov Vladimir <vladimir.fodorow@gmail.com>
2022-06-30 01:14:33 +0700

    Initial Commit

---
+++ hackpoint/hackpointApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackpointApp.swift
+//  hackpoint
+//
+//  Created by Владимир Федоров on 30.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct hackpointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
